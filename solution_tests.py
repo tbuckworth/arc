329e70d251afcef888b97d30287d24e89729c3f4
tbuckworth <elkilesh@hotmail.co.uk>
2024-12-03 12:12:55 +0000

    created solution_test.py

--- solution_tests.py
+++ solution_tests.py
@@ -1,21 +1,10 @@
-import copy
 import unittest
 
-import numpy as np
-
-from main import load_task, FOL2grid, FOL2prolog, prolog2FOL_array, tasks
+from main import load_task, tasks
 from task import Task
 
 
 class TaskTester(unittest.TestCase):
-    # @classmethod
-    # def setUpClass(cls):
-    #     cls.task_dict = load_task("data/training/d4f3cd78.json")
-    #     cls.task = Task(cls.task_dict)
-    #     # Example of accessing input/output grids for the first example
-    #     cls.output_grid = cls.task.train_examples[0].output_grid.grid
-    #     cls.out_preds = cls.task.train_examples[0].output_grid.preds
-
 
     def try_solution(self, task_file, solution):
         task_dict = load_task(f"data/training/{task_file}")
