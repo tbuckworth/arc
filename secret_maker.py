08120dad2e11703345ea13889cf6d6d9b27e3d8a
tbuckworth <elkilesh@hotmail.co.uk>
2024-12-11 17:16:11 +0000

    created secret test for task 'pour'

--- secret_maker.py
+++ secret_maker.py
@@ -1,3 +1,5 @@
+import json
+
 import numpy as np
 
 from main import tasks, load_task
@@ -12,3 +14,12 @@ if __name__ == "__main__":
     new_grid = np.concatenate((grid[:,2:],grid[:,:2]), axis=1)
 
     out_grid = Example({"input":new_grid,"output":new_grid},0,"Secret").generate_solution("solution_2")
+
+    task_dict["test"].append(
+        {"input":new_grid.astype(np.int64).tolist(),"output":out_grid.astype(np.int64).tolist()}
+    )
+    file_path = "secret_tests/pour.json"
+    with open(file_path, 'w') as json_file:
+        json.dump(task_dict, json_file, indent=None)
+
+    print(out_grid)
---
+++ secret_tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+from main import load_task, tasks
+from task import Task
+
+
+class TaskTester(unittest.TestCase):
+
+    def try_solution(self, task_file, solution):
+        print(f"\n{solution}")
+        task_dict = load_task(f"secret_tests/{task_file}.json")
+        task = Task(task_dict)
+        res = task.try_solution(solution)
+        self.assertTrue(res)
+
+    def test_solution_1(self):
+        self.try_solution("pour", 'solution_2')
+
+    def test_solution_2(self):
+        self.try_solution(tasks["pour"], 'solution_2')
+
+if __name__ == '__main__':
+    unittest.main()
